75fb15fdd267f0deb23b1e5c59c3f467625d11d4
wsybok <wsybok@gmail.com>
2023-12-10 18:26:47 +0800

    Update welbot.py

--- welbot.py
+++ welbot.py
@@ -5,9 +5,9 @@ import os
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-
 # Get the token from an environment variable
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
