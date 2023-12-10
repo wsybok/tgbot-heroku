5bc8d92aed8ac7f56b278034a18e1cd4439832a9
wsybok <wsybok@gmail.com>
2023-12-10 21:21:53 +0800

    Update welbot.py

--- welbot.py
+++ welbot.py
@@ -1,33 +1,23 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from flask import Flask, request
 import os
-
-app = Flask(__name__)
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-# Telegram bot token from environment variable
+# Get the token from an environment variable
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
-# Set up the telegram bot application
-telegram_app = ApplicationBuilder().token(TOKEN).build()
-telegram_app.add_handler(CommandHandler("hello", hello))
+# Your webhook URL
+WEBHOOK_URL = f'https://tg.typetop.xyz/{TOKEN}'
 
-# Webhook endpoint
-@app.route(f'/{TOKEN}', methods=['POST'])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), telegram_app.bot)
-    asyncio.create_task(telegram_app.update_queue.put(update))
-    return 'OK'
+async def set_webhook(app: ApplicationBuilder):
+    # Set webhook
+    await app.bot.set_webhook(WEBHOOK_URL)
 
-if __name__ == "__main__":
-    telegram_app.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get('PORT', 5000)),
-        url_path=TOKEN,
-        webhook_url=f"https://{os.environ.get('welbot')}.herokuapp.com/{TOKEN}"
-    )
-    app.run(debug=True)
+app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("hello", hello))
+
+# Run the webhook setup function
+app.run(set_webhook(app))
